a881df982846f899eeac2b0d42ed9fd776c4119b
huda-7077 <85928499+huda-7077@users.noreply.github.com>
2024-12-09 15:34:13 +0700

    feat: forgot password

---
+++ src/services/auth/forgot-password.service.ts
@@ -0,0 +1,33 @@
+import { User } from "@prisma/client";
+import { sign } from "jsonwebtoken";
+import { BASE_URL_FE, JWT_SECRET_FORGOT_PASSWORD } from "../../config";
+import { prisma } from "../../lib/prisma";
+import { transporter } from "../../lib/nodemailer";
+
+export const forgotPasswordService = async (body: Pick<User, "email">) => {
+  try {
+    const { email } = body;
+    const user = await prisma.user.findFirst({
+      where: { email },
+    });
+    if (!user) {
+      throw new Error("Invalid email address");
+    }
+
+    const token = sign({ id: user.id }, JWT_SECRET_FORGOT_PASSWORD!, {
+      expiresIn: "15m",
+    });
+
+    const link = `${BASE_URL_FE}/reset-password/${token}`;
+
+    transporter.sendMail({
+      to: email,
+      subject: "Link Reset Password",
+      html: `<a href="${link}" target="_blank">Reset password here</a>`,
+    });
+
+    return { message: "Email sent success" };
+  } catch (error) {
+    throw error;
+  }
+};
